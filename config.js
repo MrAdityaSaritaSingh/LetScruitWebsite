1450a45c874a385324e912201f977db098c297fb
Aditya S Rathore <191357538+MrAdityaSaritaSingh@users.noreply.github.com>
2026-03-28 13:21:36 +0530

    feat: move Google Apps Script URL to external config file

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+    // Replace this URL with your latest Google Apps Script Web App URL
+    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyRX116kTCMXXpyjuxREHOSMmblv6DttefMDQvdMFG81M9DygWA58AfJqXnioxwOx1t1A/exec'
+};
